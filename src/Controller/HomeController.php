7ebed1a217c9c17d24cd475c1d405d540591a1e5
baaziz <hananedu37@hotmail.fr>
2022-07-02 12:52:53 +0200

    hasher le mot de passe

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,20 +5,28 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserType;
+use App\Security\Authenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class HomeController extends AbstractController
 {
     private $entityManager;
-    private $repo;
-    public function __construct(EntityManagerInterface $entityManager)
+    private $passwordHasher;
+    private $userAuthenticatorInterface;
+    private $authenticator;
+
+    public function __construct(EntityManagerInterface $entityManager , UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticatorInterface, Authenticator $authenticator )
     {
         $this->entityManager = $entityManager;
-        $this->repo = $entityManager->getRepository(User::class);
+        $this->passwordHasher = $userPasswordHasher;
+        $this->authenticator = $authenticator;
+        $this->userAuthenticatorInterface = $userAuthenticatorInterface;
     }
 
     /**
@@ -45,16 +53,19 @@ class HomeController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $user = $form->getData();
+            $passwordHashed = $this->passwordHasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($passwordHashed);
+            $user->setRoles(["ROLE_SOCIETE"]);
 
             $this->entityManager->persist($user);
             $this->entityManager->flush();
-            return $this->redirectToRoute("app_login");
+            return $this->userAuthenticatorInterface->authenticateUser($user, $this->authenticator, $request);
         }
 
-        return $this->render(
-            'home/index.html.twig',
-            ['form' => $form->createView(),
-               ]
+        return $this->render('home/index.html.twig',
+            [
+                'form' => $form->createView(),
+            ]
 
         );
     }
